30d85c0f3693b93d548f8a5cbe9d5d40d1927576
AHMAD BANI YASEEN <ahmadbaniyassen8@gmail.com>
2022-11-01 22:49:58 +0200

    hi

--- covidvaccineAPI.INFRA/Repository/UserAccountRepository.cs
+++ covidvaccineAPI.INFRA/Repository/UserAccountRepository.cs
@@ -45,7 +45,8 @@ namespace covidvaccineAPI.INFRA.Repository
 
         public List<Useraccount> firstvaccine()
         {
-            throw new NotImplementedException();
+            IEnumerable<Useraccount> result = _dbContext.Connection.Query<Useraccount>("User_Package.firstvaccine", commandType: System.Data.CommandType.StoredProcedure);
+            return result.ToList();
         }
 
         public List<Useraccount> GetAllUsers()
@@ -56,22 +57,39 @@ namespace covidvaccineAPI.INFRA.Repository
 
         public Useraccount GetUserById(int id)
         {
-            throw new NotImplementedException();
+            var p = new DynamicParameters();
+            p.Add("id", id, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            IEnumerable<Useraccount> result = _dbContext.Connection.Query<Useraccount>("User_Package.GetUserById", commandType: System.Data.CommandType.StoredProcedure);
+            return result.FirstOrDefault();
+
         }
 
         public List<Useraccount> NotTokeVaccine()
         {
-            throw new NotImplementedException();
+            IEnumerable<Useraccount> result = _dbContext.Connection.Query<Useraccount>("User_Package.NotTokeVaccine", commandType: System.Data.CommandType.StoredProcedure);
+            return result.ToList();
         }
 
         public List<Useraccount> Secondvaccine()
         {
-            throw new NotImplementedException();
+            IEnumerable<Useraccount> result = _dbContext.Connection.Query<Useraccount>("User_Package.Secondvaccine", commandType: System.Data.CommandType.StoredProcedure);
+            return result.ToList();
         }
 
         public void UpdateUser(Useraccount useraccount)
         {
-            throw new NotImplementedException();
+            var p = new DynamicParameters();
+            p.Add("Full_Name", useraccount.Fullname, dbType: DbType.String, direction: ParameterDirection.Input);
+            p.Add("User_Name", useraccount.Username, dbType: DbType.String, direction: ParameterDirection.Input);
+            p.Add("Phone", useraccount.Phonenumber, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            p.Add("Number_of_Vaccines", useraccount.Numberofvaccines, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            p.Add("ImagePath", useraccount.Image, dbType: DbType.String, direction: ParameterDirection.Input);
+            p.Add("Email_User", useraccount.Email, dbType: DbType.String, direction: ParameterDirection.Input);
+            p.Add("Password_User", useraccount.Password, dbType: DbType.String, direction: ParameterDirection.Input);
+            p.Add("Age_User", useraccount.Age, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            p.Add("Role_id", useraccount.Roleid, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            var result = _dbContext.Connection.Execute("User_Package.UpdateUser", p, commandType: CommandType.StoredProcedure);
+
         }
     }
 }
