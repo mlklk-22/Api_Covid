c76dfb2128375b659867d5f5a8622b23b2f2fcd0
abdelraheem4 <aarabiyyat85@gmail.com>
2022-11-02 00:37:22 +0300

    generic service

---
+++ covidvaccineAPI.CORE/Service/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace covidvaccineAPI.CORE.Service
+{
+    public interface IGenericService<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        
+    }
+}
