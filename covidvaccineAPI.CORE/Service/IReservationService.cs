fc956e838e77771b7cd13efc9a4ebca9452e7b23
mlklk-22 <106337311+mlklk-22@users.noreply.github.com>
2022-11-03 01:22:00 +0300

    Abdulmalek

---
+++ covidvaccineAPI.CORE/Service/IReservationService.cs
@@ -0,0 +1,16 @@
+﻿using covidvaccineAPI.CORE.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covidvaccineAPI.CORE.Service
+{
+    public interface IReservationService
+    {
+        void CreateReservation(Reservation reservation);
+        List<Reservation> GetAllReservation();
+        Reservation GetReservationById(int id);
+        void UpdateReservation(Reservation reservation);
+        void DeleteReservation(int id);
+    }
+}
---
+++ covidvaccineAPI.CORE/Service/ITestmonialService.cs
@@ -0,0 +1,16 @@
+﻿using covidvaccineAPI.CORE.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covidvaccineAPI.CORE.Service
+{
+    public interface ITestmonialService
+    {
+        void CreateTestmonial(Testmonial reservation);
+        List<Testmonial> GetAllTestmonial();
+        Testmonial GetTestmonialById(int id);
+        void UpdateTestmonial(Testmonial reservation);
+        void DeleteTestmonial(int id);
+    }
+}
