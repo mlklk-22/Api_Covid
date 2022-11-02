ed314daa020efb6015430645e2940585caba8251
mlklk-22 <106337311+mlklk-22@users.noreply.github.com>
2022-11-03 01:25:28 +0300

    Abdulmalek

---
+++ covidvaccineAPI.INFRA/Service/ReservationService.cs
@@ -0,0 +1,44 @@
+﻿using covidvaccineAPI.CORE.Data;
+using covidvaccineAPI.CORE.Repository;
+using covidvaccineAPI.CORE.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covidvaccineAPI.INFRA.Service
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly IReservationRepository _reservationRepository;
+
+        public ReservationService(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+        public void CreateReservation(Reservation reservation)
+        {
+            _reservationRepository.CreateReservation(reservation);
+        }
+
+        public void DeleteReservation(int id)
+        {
+           _reservationRepository.DeleteReservation(id);
+        }
+
+        public List<Reservation> GetAllReservation()
+        {
+            return _reservationRepository.GetAllReservation();
+        }
+
+        public Reservation GetReservationById(int id)
+        {
+            return _reservationRepository.GetReservationById(id);
+        }
+
+        public void UpdateReservation(Reservation reservation)
+        {
+            _reservationRepository.UpdateReservation(reservation);
+        }
+    }
+}
---
+++ covidvaccineAPI.INFRA/Service/TestmonialService.cs
@@ -0,0 +1,44 @@
+﻿using covidvaccineAPI.CORE.Data;
+using covidvaccineAPI.CORE.Repository;
+using covidvaccineAPI.CORE.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covidvaccineAPI.INFRA.Service
+{
+    public class TestmonialService : ITestmonialService
+    {
+        private readonly ITestmonialRepository _testmonialRepository;
+
+        public TestmonialService(ITestmonialRepository testmonialRepository)
+        {
+            _testmonialRepository = testmonialRepository;
+        }
+
+        public void CreateTestmonial(Testmonial reservation)
+        {
+            _testmonialRepository.CreateTestmonial(reservation);
+        }
+
+        public void DeleteTestmonial(int id)
+        {
+            _testmonialRepository.DeleteTestmonial(id);
+        }
+
+        public List<Testmonial> GetAllTestmonial()
+        {
+           return _testmonialRepository.GetAllTestmonial();
+        }
+
+        public Testmonial GetTestmonialById(int id)
+        {
+            return _testmonialRepository.GetTestmonialById(id);
+        }
+
+        public void UpdateTestmonial(Testmonial reservation)
+        {
+            _testmonialRepository.UpdateTestmonial(reservation);
+        }
+    }
+}
