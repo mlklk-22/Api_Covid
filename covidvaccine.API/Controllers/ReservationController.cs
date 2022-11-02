44d1c0582e75495f789288544162bdc4c33352d1
mlklk-22 <106337311+mlklk-22@users.noreply.github.com>
2022-11-03 02:17:23 +0300

    Abdulmalek

---
+++ covidvaccine.API/Controllers/ReservationController.cs
@@ -0,0 +1,47 @@
+﻿using covidvaccineAPI.CORE.Data;
+using covidvaccineAPI.CORE.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace covidvaccine.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+
+        public ReservationController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+        [HttpPost]
+        public void CreateReservation(Reservation reservation)
+        {
+            _reservationService.CreateReservation(reservation);
+        }
+        [HttpDelete]
+        public void DeleteReservation(int id)
+        {
+            _reservationService.DeleteReservation(id);
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Reservation> GetAllReservation()
+        {
+            return _reservationService.GetAllReservation();
+        }
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public Reservation GetReservationById(int id)
+        {
+            return _reservationService.GetReservationById(id);
+        }
+        [HttpPut]
+        public void UpdateReservation(Reservation reservation)
+        {
+            _reservationService.UpdateReservation(reservation);
+        }
+    }
+}
---
+++ covidvaccine.API/Controllers/TestmonialController.cs
@@ -0,0 +1,47 @@
+﻿using covidvaccineAPI.CORE.Data;
+using covidvaccineAPI.CORE.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace covidvaccine.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestmonialController : ControllerBase
+    {
+        private readonly ITestmonialService _testmonialService;
+
+        public TestmonialController(ITestmonialService testmonialService)
+        {
+            _testmonialService = testmonialService;
+        }
+        [HttpPost]
+        public void CreateTestmonial(Testmonial testmonial)
+        {
+            _testmonialService.CreateTestmonial(testmonial);
+        }
+        [HttpDelete]
+        public void DeleteTestmonial(int id)
+        {
+            _testmonialService.DeleteTestmonial(id);
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Testmonial> GetAllTestmonial()
+        {
+            return _testmonialService.GetAllTestmonial();
+        }
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public Testmonial GetTestmonialById(int id)
+        {
+            return _testmonialService.GetTestmonialById(id);
+        }
+        [HttpPut]
+        public void UpdateTestmonial(Testmonial testmonial)
+        {
+            _testmonialService.UpdateTestmonial(testmonial);
+        }
+    }
+}
