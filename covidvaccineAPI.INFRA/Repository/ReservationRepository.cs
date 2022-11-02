ba24bf51d165dc996d0a4df4e611327c278d283f
mlklk-22 <106337311+mlklk-22@users.noreply.github.com>
2022-11-03 01:24:43 +0300

    Abdulmalek

---
+++ covidvaccineAPI.INFRA/Repository/ReservationRepository.cs
@@ -0,0 +1,62 @@
+﻿using covidvaccineAPI.CORE.Common;
+using covidvaccineAPI.CORE.Data;
+using covidvaccineAPI.CORE.Repository;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace covidvaccineAPI.INFRA.Repository
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        private readonly IDbContext _dbContext;
+
+        public ReservationRepository(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void CreateReservation(Reservation reservation)
+        {
+            var p = new DynamicParameters();
+            p.Add("ActualDateDose1", reservation.Actualdatedose1, dbType: DbType.Date, direction: ParameterDirection.Input);
+            p.Add("ActualDateDose2", reservation.Actualdatedose2, dbType: DbType.Date, direction: ParameterDirection.Input);
+            _dbContext.Connection.Execute("RESERVATION_PACKAGE.CreateReservation", p, commandType: CommandType.StoredProcedure);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            var p = new DynamicParameters();
+            p.Add("Id", id, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            _dbContext.Connection.Execute("RESERVATION_PACKAGE.DeleteReservation", p, commandType: CommandType.StoredProcedure);
+
+        }
+
+        public List<Reservation> GetAllReservation()
+        {
+            IEnumerable<Reservation> result = _dbContext.Connection.Query<Reservation>("RESERVATION_PACKAGE.GetAllReservation", commandType: CommandType.StoredProcedure);
+            return result.ToList();
+        }
+
+        public Reservation GetReservationById(int id)
+        {
+            var p = new DynamicParameters();
+            p.Add("Id", id, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            IEnumerable<Reservation> result = _dbContext.Connection.Query<Reservation>("RESERVATION_PACKAGE.GetReservationById", p, commandType: CommandType.StoredProcedure);
+            return result.FirstOrDefault();
+
+        }
+
+        public void UpdateReservation(Reservation reservation)
+        {
+            var p = new DynamicParameters();
+            p.Add("Id", reservation.Reserveid, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            p.Add("Actualdatedose1", reservation.Actualdatedose1, dbType: DbType.Date, direction: ParameterDirection.Input);
+            p.Add("Actualdatedose2", reservation.Actualdatedose2, dbType: DbType.Date, direction: ParameterDirection.Input);
+            _dbContext.Connection.Execute("RESERVATION_PACKAGE.UpdateReservation", p, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
---
+++ covidvaccineAPI.INFRA/Repository/TestmonialRepository.cs
@@ -0,0 +1,53 @@
+﻿using covidvaccineAPI.CORE.Common;
+using covidvaccineAPI.CORE.Data;
+using covidvaccineAPI.CORE.Repository;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace covidvaccineAPI.INFRA.Repository
+{
+    public class TestmonialRepository : ITestmonialRepository
+    {
+        private readonly IDbContext _dbContext;
+
+        public void CreateTestmonial(Testmonial testmonial)
+        {
+            var p = new DynamicParameters();
+            p.Add("Message", testmonial.Message, dbType: DbType.String, direction: ParameterDirection.Input);
+            _dbContext.Connection.Execute("TESTMONIAL_PACKAGE.CreateTestmonial", p, commandType: CommandType.StoredProcedure);
+        }
+
+        public void DeleteTestmonial(int id)
+        {
+            var p = new DynamicParameters();
+            p.Add("Id", id, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            _dbContext.Connection.Execute("TESTMONIAL_PACKAGE.DeleteTestmonial", p, commandType: CommandType.StoredProcedure);
+        }
+
+        public List<Testmonial> GetAllTestmonial()
+        {
+            IEnumerable<Testmonial> result = _dbContext.Connection.Query<Testmonial>("TESTMONIAL_PACKAGE.GetAllTestmonial", commandType: CommandType.StoredProcedure);
+            return result.ToList();
+        }
+
+        public Testmonial GetTestmonialById(int id)
+        {
+            var p = new DynamicParameters();
+            p.Add("Id", id, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            IEnumerable<Testmonial> result = _dbContext.Connection.Query<Testmonial>("TESTMONIAL_PACKAGE.GetTestmonialById", p, commandType: CommandType.StoredProcedure);
+            return result.FirstOrDefault();
+        }
+
+        public void UpdateTestmonial(Testmonial testmonial)
+        {
+            var p = new DynamicParameters();
+            p.Add("Id", testmonial.Idtest, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            p.Add("Message", testmonial.Message, dbType: DbType.String, direction: ParameterDirection.Input);
+            _dbContext.Connection.Execute("TESTMONIAL_PACKAGE.UpdateTestmonial", p, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
