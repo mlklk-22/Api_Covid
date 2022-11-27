7f96f553335cd0432f4b9d5de845b6adfe920f5f
mlklk-22 <mlkmsbh84@gmail.com>
2022-11-27 21:40:11 +0300

    abd

---
+++ covidvaccineAPI.CORE/DATA/Home.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace covidvaccineAPI.CORE.Data
+{
+    public partial class Home
+    {
+        public decimal Homeid { get; set; }
+        public string Imagelogo { get; set; }
+        public string Littletitle1 { get; set; }
+        public string Title1 { get; set; }
+        public string Descriptiontitle1 { get; set; }
+        public string Imagetitle1 { get; set; }
+        public string Textbutton { get; set; }
+        public string Statisticstitle { get; set; }
+        public string Descriptionstatistics { get; set; }
+        public string Desc1statistics { get; set; }
+        public string Desc2statistics { get; set; }
+        public string Desc3statistics { get; set; }
+        public string Newstitle { get; set; }
+        public string Newsimage1 { get; set; }
+        public string Newsimage2 { get; set; }
+        public string Newsimage3 { get; set; }
+        public string Newsdesc1 { get; set; }
+        public string Newsdesc2 { get; set; }
+        public string Newsdesc3 { get; set; }
+    }
+}
---
+++ covidvaccineAPI.CORE/Repository/IHomeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covidvaccineAPI.CORE.Repository
+{
+    public interface IHomeRepository
+    {
+    }
+}
